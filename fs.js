85b1e23e9740ea8bef4438e843ab2751951011d0
Egamberdiyev-Akmal <egamberdiyevakmal2000@gmail.com>
2024-09-25 09:22:04 +0500

    Module Operation System. OS

--- fs.js
+++ fs.js
@@ -16,9 +16,13 @@ fs.writeFile(path.join(__dirname, 'nodes', 'december.txt'), 'Create new coure No
     if (err) throw new Error()
     console.log('File was creade successfully')
 
-    fs.appendFile(path.join(__dirname, 'nodes', 'december.txt'), 'and project', err => {
+    fs.appendFile(path.join(__dirname, 'nodes', 'december.txt'), ' and project', err => {
         if (err) throw new Error()
-
         console.log('successe fully')
+
+        fs.readFile(path.join(__dirname, 'nodes', 'december.txt'), "utf-8", (err, data) => {
+            if (err) throw new Error()
+            console.log(data)
+        })
     })
 })
