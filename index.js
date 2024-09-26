b88b76a9ca51ff21df7474221c7cd3043b94cda6
Egamberdiyev-Akmal <egamberdiyevakmal2000@gmail.com>
2024-09-26 21:31:31 +0500

    M

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const http = require('http')
 const server = http.createServer((request, response) => {
-    if (request.method === "GET")
+    if (request.method === "GET") {
+
         response.writeHead(200, { 'Content-type': 'text/html' })
-    response.end(`
-    <h1>Send email</h1>
-    <form method="post" action="/">
-     <input name="email" type="email">
-     <button type="submit">Send email</button>
-    </form>
-    `)
+        response.end(`
+            <h1>Send email</h1>
+            <form method="post" action="/">
+            <input name="email" type="email">
+            <button type="submit">Send email</button>
+            </form>
+            `)
+    } else if (request.method === "POST") {
+        response.end("Email successfully added")
+    }
 })
 
 server.listen(3000, () => {
