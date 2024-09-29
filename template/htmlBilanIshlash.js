6e8c889be4178bff01455ac7c94547c4ac1e587b
Egamberdiyev-Akmal <egamberdiyevakmal2000@gmail.com>
2024-09-29 20:14:04 +0500

    html bilan ishlash

--- template/htmlBilanIshlash.js
+++ template/htmlBilanIshlash.js
@@ -1,27 +1,27 @@
-// const http = require('http')
-// const path = require('path')
-// const fs = require('fs')
-// const server = http.createServer((request, response) => {
-//     if (request.method === "GET") {
-//         response.writeHead(200, { 'Content-type': 'text/html' })
-//         if (request.url === '/') {
-//             fs.readFile(path.join(__dirname, 'template', 'index.html'), 'utf-8', (err, content) => {
-//                 if (err) throw err
-//                 request.end(content)
-//             })
-//         }
-//     } else if (request.method === "POST") {
-//         const body = []
-//         request.on('data', data => {
-//             body.push(Buffer.from(data))
-//         })
-//         request.on('end', () => {
-//             const messages = body.toString().split('=')[1]
-//             response.end(`Email successfully added: ${messages}`)
-//         })
-//     }
-// })
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+const server = http.createServer((request, response) => {
+    if (request.method === "GET") {
+        response.writeHead(200, { 'Content-type': 'text/html' })
+        if (request.url === '/') {
+            fs.readFile(path.join(__dirname, 'template', 'index.html'), 'utf-8', (err, content) => {
+                if (err) throw err
+                request.end(content)
+            })
+        }
+    } else if (request.method === "POST") {
+        const body = []
+        request.on('data', data => {
+            body.push(Buffer.from(data))
+        })
+        request.on('end', () => {
+            const messages = body.toString().split('=')[1]
+            response.end(`Email successfully added: ${messages}`)
+        })
+    }
+})
 
-// server.listen(5000, () => {
-//     console.log('Server has been started on port:3000')
-// })
+server.listen(5000, () => {
+    console.log('Server has been started on port:3000')
+})
